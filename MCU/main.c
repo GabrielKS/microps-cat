0ecf3f1d5ab38fb849c40a6a967def7ea65e3e0b
ping48 <anthonymkang@gmail.com>
2022-11-14 20:05:38 -0800

    yuh

--- MCU/main.c
+++ MCU/main.c
@@ -1,34 +1,147 @@
-/*********************************************************************
-*                    SEGGER Microcontroller GmbH                     *
-*                        The Embedded Experts                        *
-**********************************************************************
-
--------------------------- END-OF-HEADER -----------------------------
-
-File    : main.c
-Purpose : Generic application start
-
+/*
+File: Lab_6_JHB.c
+Author: Josh Brake
+Email: jbrake@hmc.edu
+Date: 9/14/19
 */
+ // http://192.168.4.1/.
 
-#include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
 
-/*********************************************************************
-*
-*       main()
-*
-*  Function description
-*   Application entry point.
-*/
-int main(void) {
-  int i;
+/////////////////////////////////////////////////////////////////
+// Provided Constants and Functions
+/////////////////////////////////////////////////////////////////
 
-  for (i = 0; i < 100; i++) {
-    printf("Hello World %d!\n", i);
-  }
-  do {
-    i++;
-  } while (1);
+//Defining the web page in two chunks: everything before the current time, and everything after the current time
+char* webpageStart = "<!DOCTYPE html><html><head><title>E155 Web Server Demo Webpage</title>\
+	<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\
+	</head>\
+	<body><h1>E155 Web Server Demo Webpage</h1>";
+char* ledStr = "<p>LED Control:</p><form action=\"ledon\"><input type=\"submit\" value=\"Turn the LED on!\"></form>\
+	<form action=\"ledoff\"><input type=\"submit\" value=\"Turn the LED off!\"></form>";
+char* webpageEnd   = "</body></html>";
+
+//determines whether a given character sequence is in a char array request, returning 1 if present, -1 if not present
+int inString(char request[], char des[]) {
+	if (strstr(request, des) != NULL) {return 1;}
+	return -1;
 }
 
-/*************************** End of file ****************************/
+int updateLEDStatus(char request[])
+{
+	int led_status = 0;
+	// The request has been received. now process to determine whether to turn the LED on or off
+	if (inString(request, "ledoff")==1) {
+		digitalWrite(LED_PIN, PIO_LOW);
+		led_status = 0;
+	}
+	else if (inString(request, "ledon")==1) {
+		digitalWrite(LED_PIN, PIO_HIGH);
+		led_status = 1;
+	}
+
+	return led_status;
+}
+
+/////////////////////////////////////////////////////////////////
+// Solution Functions
+/////////////////////////////////////////////////////////////////
+
+int main(void) {
+  configureFlash();
+  configureClock();
+
+  gpioEnable(GPIO_PORT_A);
+  gpioEnable(GPIO_PORT_B);
+  gpioEnable(GPIO_PORT_C);
+
+  pinMode(PB3, GPIO_OUTPUT);
+  
+  RCC->APB2ENR |= (RCC_APB2ENR_TIM15EN);
+  initTIM(TIM15);
+  
+  USART_TypeDef * USART = initUSART(USART1_ID, 125000);
+
+  //SPI initialization code
+  int br, cpol,cpha;
+  //TODO set right vals
+  br = 0b111;
+  cpol = 0;
+  cpha = 1;
+  initSPI(br, cpol, cpha);
+  // test with RIGOL
+  /*while(1) {
+    // write the byte to configure register
+    digitalWrite(PA8, 1); // enable chip
+    spiSendReceive(0x80); // address of config register
+    spiSendReceive(0b11100000); // configure temp sensor register
+    digitalWrite(PA8, 0); // disable chip
+    // read the byte for temperature
+    digitalWrite(PA8,1);  // enable chip
+    spiSendReceive(0x02);  // set the starting address of data of MSB
+    signed char temp_MSB =  spiSendReceive(0); // store the read back MSB
+    digitalWrite(PA8, 0);  // disable chip
+  }*/
+  while(1) {
+    /* Wait for ESP8266 to send a request.
+    Requests take the form of '/REQ:<tag>\n', with TAG begin <= 10 characters.
+    Therefore the request[] array must be able to contain 18 characters.
+    */
+
+    // SPI code here for reading temperature
+    // configTemp();
+    // write the byte to configure register
+    digitalWrite(PA8, 1); // enable chip
+    spiSendReceive(0x80); // address of config register
+    spiSendReceive(0b11100000); // configure temp sensor register
+    digitalWrite(PA8, 0); // disable chip
+    // read the byte for temperature
+    digitalWrite(PA8,1);  // enable chip
+    spiSendReceive(0x02);  // set the starting address of data of MSB
+    signed char temp_MSB =  spiSendReceive(0); // store the read back MSB
+    digitalWrite(PA8, 0);  // disable chip
+    // Receive web request from the ESP
+    char request[BUFF_LEN] = "                  "; // initialize to known value
+    int charIndex = 0;
+  
+    // Keep going until you get end of line character
+    while(inString(request, "\n") == -1) {
+      // Wait for a complete request to be transmitted before processing
+      while(!(USART->ISR & USART_ISR_RXNE));
+      request[charIndex++] = readChar(USART);
+    }
+  
+    // Update string with current LED state
+  
+    int led_status = updateLEDStatus(request);
+
+    char ledStatusStr[20];
+    if (led_status == 1)
+      sprintf(ledStatusStr,"LED is on!");
+    else if (led_status == 0)
+      sprintf(ledStatusStr,"LED is off!");
+    // make temperature string
+    //testing 
+    char tempString[256];
+    sprintf(tempString, "The temperature is: %d Celcius ", temp_MSB);
+
+    // finally, transmit the webpage over UART
+    sendString(USART, webpageStart); // webpage header code
+    sendString(USART, ledStr); // button for controlling LED
+
+    sendString(USART, "<h2>LED Status</h2>");
+
+
+    sendString(USART, "<p>");
+    sendString(USART, ledStatusStr);
+    sendString(USART, "</p>");
+    sendString(USART,"<p>");
+    sendString(USART, tempString);
+    sendString(USART, "</p>");
+  
+    sendString(USART, webpageEnd);
+  }
+}
